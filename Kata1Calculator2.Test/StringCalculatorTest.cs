5725636419867fe86863d0dc059046168330f83b
Nkazimulo Siyaya <nkazimulo.siyaya@kcpivot.co.za>
2022-02-07 13:49:45 +0200

    forth attempt

---
+++ Kata1Calculator2.Test/StringCalculatorTest.cs
@@ -0,0 +1,127 @@
+﻿using NUnit.Framework;
+
+
+namespace Kata1Calculator2.Test
+{
+    public class StringCalculatorTest
+    {
+        private StringCalculator _stringCalculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _stringCalculator = new StringCalculator();
+        }
+
+        [Test]
+        public void WhenEnteringEmptyString_UsingAdd_ResultsReturnsZero()
+        {
+            // arrange
+            const int expected = 0;
+            const string input = "";
+
+            // act 
+            var results = _stringCalculator.Add(input);
+
+            // assert
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void WhenEnteringStringWithOneNumber_UsingAdd_ResultsReturnsOne()
+        {
+            // arrange
+            const int expected = 1;
+            const string input = "1";
+
+            // act 
+            var results = _stringCalculator.Add(input);
+
+            // assert
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void WhenEnteringStringWithTwoNumber_UsingAdd_ResultsReturnsThree()
+        {
+            // arrange
+            const int expected = 3;
+            const string input = "1,2";
+
+            // act 
+            var results = _stringCalculator.Add(input);
+
+            // assert
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void WhenEnteringStringWithNextLine_UsingAdd_ResultsReturnsSix()
+        {
+            // arrange
+            const int expected = 6;
+            const string input = "1,2\n3";
+
+            // act 
+            var results = _stringCalculator.Add(input);
+
+            // assert
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void WhenEnteringStringWithMoreNumbers_UsingAdd_ResultsReturns15()
+        {
+            // arrange
+            const int expected = 15;
+            const string input = "1,2\n3,4,5";
+
+            // act 
+            var results = _stringCalculator.Add(input);
+
+            // assert
+            Assert.AreEqual(expected, results);
+        }
+        [Test]
+        public void WhenEnteringStringWithANewDelimiter_UsingAdd_ResultsReturnsThree()
+        {
+            // arrange
+            const int expected = 3;
+            const string input = "//;\n1;2";
+
+            // act 
+            var results = _stringCalculator.Add(input);
+
+            // assert
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void WhenEnteringStringWithNumberGreaterThanOneThousand_UsingAdd_ResultsReturnsTwo()
+        {
+            // arrange
+            const int expected = 2;
+            const string input = "1000,2";
+
+            // act 
+            var results = _stringCalculator.Add(input);
+
+            // assert
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void WhenEnteringStringWithDelimiterOfAnyLength_UsingAdd_ResultsReturnsSix()
+        {
+            // arrange
+            const int expected = 6;
+            const string input = "//***\n1***2***3";
+
+            // act 
+            var results = _stringCalculator.Add(input);
+
+            // assert
+            Assert.AreEqual(expected, results);
+        }
+    }
+}
---
+++ Kata1Calculator2/StringCalculator.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+namespace Kata1Calculator2
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            var delimiter = "";
+            if (string.IsNullOrEmpty(numbers))
+            {
+                return 0;
+            }
+
+            numbers = RemoveSlash(numbers); 
+            delimiter = GetDelimiter(numbers);
+            string[] splitNumbers = numbers.Split(new string[] { ",", "\n", delimiter }, StringSplitOptions.RemoveEmptyEntries);
+            var convectedNumbers = ConvertStringArrayToIntArray(splitNumbers);
+
+            return SumOfNumbers(convectedNumbers);
+        }
+        private string RemoveSlash(string number)
+        {
+            if (number.Contains("//"))
+            {
+                number = number.Substring(2);
+            }
+
+            return number;
+        }
+
+        private string GetDelimiter(string numbers)
+        {
+            var delimiter = "";
+            var count = 1;
+            if (!Char.IsDigit(numbers[0]))
+            {
+                delimiter = numbers[0].ToString();
+
+                while (!Char.IsDigit(numbers[count]) && numbers[count] != '\n')
+                {
+                    delimiter = delimiter + numbers[count].ToString();
+                    count++;
+                }
+            }
+
+            return delimiter;
+        }
+
+        private int[] ConvertStringArrayToIntArray(string[] numbers)
+        {
+            var result = new int[numbers.Length];
+
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                result[i] = Convert.ToInt32(numbers[i]);
+            }
+
+            return result;
+        }
+
+        private int SumOfNumbers(int[] numbers)
+        {
+            var sumOfNumbers = 0;
+
+            foreach (int number in numbers)
+            {
+                if(number < 0)
+                {
+                    negativeNumbers(numbers);
+                }
+                else if(number < 1000)
+                {
+                    sumOfNumbers = sumOfNumbers + number;
+                }
+                
+            }
+
+            return sumOfNumbers;
+        }
+
+        private List<int> negativeNumbers(int[] numbers)
+        {
+            throw new NotImplementedException("negatives not allowed");
+            List<int> negativeNumbers = new List<int>();
+            foreach (int number in numbers)
+            {
+                if (number < 0)
+                {
+                  negativeNumbers.Add(number);
+                }
+            }
+            return negativeNumbers;
+        }
+    }
+}
